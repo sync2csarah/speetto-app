a8ce13f394507240c00becdd0e287931c15f29ef
sync2csarah <jysarahtiny@gmail.com>
2026-09-18 00:12:08 +0900

    Update TodayFortune.js

--- src/components/TodayFortune.js
+++ src/components/TodayFortune.js
@@ -19,7 +19,7 @@ function getTodayFortune() {
   return FORTUNES[seed % FORTUNES.length];
 }
 
-const API_BASE = "http://localhost:8000";
+const API_BASE = process.env.REACT_APP_API_URL || "http://localhost:8000";
 
 export default function TodayFortune() {
   const [alerts, setAlerts] = useState([]);
